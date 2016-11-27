2acfecf0c5848fa21554efdea8fa12923769c3d8
Andreas Rousing <arousing@princeton.edu>
2016-11-27 01:04:48 -0500

    Updated plotArray.m

--- plotArray.m
+++ plotArray.m
@@ -1,10 +1,11 @@
 %% Plot Array
-File = 'data/temp_array_serial_100.txt';
+File = 'data/temparray_serial_512.txt';
 A = dlmread(File,' ',3,2);
-nx=sqrt(length(A));
+nx=ceil(sqrt(length(A)));
 T=reshape(A,[nx,nx])'
 
 image(T)
 image(T,'CDataMapping','scaled')
 colorbar
-tmp_avg=sum(A)/nx^2
+tmp_avg=sum(A)/nx^2;
+title(['Serial nx=' num2str(nx) ' t\_avg=' num2str(tmp_avg)]);
